0acd766903db86c631d1235d0968cd0848589f2c
MAshar <ashar@claryicon.com>
2024-03-01 09:13:48 +0500

    Handling pm2.

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from spoken_to_signed.gloss_to_pose import gloss_to_pose, CSVPoseLookup
 from dotenv import load_dotenv
 from io import BytesIO
 from flask_compress import Compress
+from gunicorn.app.base import BaseApplication
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,6 +13,8 @@ Compress(app)
 
 @app.route('/spoken_text_to_signed_pose', methods=['GET'])
 def text_to_glosses():
+    worker_pid = os.getpid()
+    print(f"Worker PID: {worker_pid}")
     try:
         text = request.args.get('text').lower()
         language = request.args.get('spoken')
@@ -50,7 +53,33 @@ def text_to_glosses():
         return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
-    port_number = int(os.getenv('PORT', 3002))
-    ssl_context = (os.getenv('CERTIFICATE_PATH'), os.getenv('PRIVATE_KEY_PATH'))
-    app.run(debug=True, host='0.0.0.0', port=port_number, ssl_context=ssl_context)
-    print(f"Server listening on port: {port_number}")
+    if os.getenv('PM2_HOME'):
+        port_number = int(os.getenv('PORT', 3002))
+        ssl_context = (os.getenv('CERTIFICATE_PATH'), os.getenv('PRIVATE_KEY_PATH'))
+        options = {
+            'bind': f'0.0.0.0:{port_number}',
+            'workers': int(os.getenv('GUNICORN_WORKERS', 2)),
+            'threads': int(os.getenv('GUNICORN_THREADS', 1)),
+            'certfile': ssl_context[0],
+            'keyfile': ssl_context[1],
+            'loglevel': 'debug'
+        }
+        class FlaskApplication(BaseApplication):
+            def __init__(self, app, options=None):
+                self.options = options or {}
+                self.application = app
+                super().__init__()
+
+            def load_config(self):
+                for key, value in self.options.items():
+                    if key in self.cfg.settings and value is not None:
+                        self.cfg.set(key.lower(), value)
+
+            def load(self):
+                return self.application
+        FlaskApplication(app, options).run()
+    else:
+        port_number = int(os.getenv('PORT', 3002))
+        ssl_context = (os.getenv('CERTIFICATE_PATH'), os.getenv('PRIVATE_KEY_PATH'))
+        app.run(debug=True, host='0.0.0.0', port=port_number, ssl_context=ssl_context)
+        print(f"Server listening on port: {port_number}")
